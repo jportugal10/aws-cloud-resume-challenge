4cf1226ac375d2ca46be49ee577491198dc04160
Jose Portugal <joseportugal@jportuglmacbook.attlocal.net>
2023-05-26 16:10:24 -0700

    Adding Terraform IaC folder for lambda python function with dynamodb

---
+++ Infrastructure/lambda/function.py
@@ -0,0 +1,18 @@
+import json
+import boto3
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('cloudresume-challenge')
+def lambda_handler(event, context):
+    response = table.get_item(Key={
+        'ID' : '0'
+    })
+    views = response[ 'Item' ][ 'views' ]
+    views = views + 1
+    print(views)
+    response = table.put_item(Item={
+        'ID' : '0' ,
+        'views' : views 
+    })
+    
+    return views
+
