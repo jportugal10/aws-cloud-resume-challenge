5ac91c5e2c3ad0edcd18cdab4a2e907a31052a07
Jose Portugal <joseportugal@jportuglmacbook.attlocal.net>
2024-10-06 10:58:51 -0700

    Updating Runtime on Terraform File

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -22,7 +22,7 @@ resource "aws_lambda_function" "myfunction" {
   function_name    = "myfunction"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "function.lambda_handler"
-  runtime          = "python3.8"
+  runtime          = "python3.12"
 }
 
 
